caa34f27fd8528c60a7ceb01908cf0135048d800
Simone Ferreira <simoneef1986@gmail.com>
2022-04-06 15:58:34 +0100

    Entity Adapter Configuration

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -1,13 +1,17 @@
 import { createEntityAdapter, EntityState } from "@ngrx/entity";
 import { createReducer, on } from "@ngrx/store";
 import { CourseActions } from "../action-types";
-import { Course } from "../model/course";
+import { compareCourses, Course } from "../model/course";
 
 export interface CoursesState extends EntityState<Course> {
   
 }
 
-export const adapter = createEntityAdapter<Course>();
+export const adapter = createEntityAdapter<Course>({
+
+    sortComparer: compareCourses
+    
+});
 
 export const initialCourseState = adapter.getInitialState();
 
@@ -18,3 +22,5 @@ export const coursesReducer =createReducer(
     on(CourseActions.allCoursesLoaded,
         (state, action) => adapter.addAll(action.courses, state))
 );
+
+export const {selectAll} = adapter.getSelectors();
